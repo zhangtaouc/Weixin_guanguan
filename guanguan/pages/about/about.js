64e014e1367efc82974ffa4f75c1b65d9ba61eac
Doublezt <39691213+Doublezt@users.noreply.github.com>
2018-08-22 22:45:44 +0800

    guanguan

---
+++ guanguan/pages/about/about.js
@@ -0,0 +1,24 @@
+Page({
+  data:{
+    // text:"这是一个页面"
+    audioPoster: 'http://img2.imgtn.bdimg.com/it/u=3597127160,3556608346&fm=27&gp=0.jpg',
+    audioName: 'come on',
+    audioAuthor: '关关',
+    audioSrc: 'http://www.ytmp3.cn/?down/49492.mp3',
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
